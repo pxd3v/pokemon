4431e136de81b7475b44a32d342f95a876a324f0
Pedro Lages <pxd3v@users.noreply.github.com>
2020-07-06 18:32:26 -0300

    [enhancement]

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
-    <h1>Teste</h1>
+    <div className="App">
+      <header className="App-header">
+        <div>a</div>      
+      </header>
+    </div>
   );
 }
 
